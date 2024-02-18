b5518761329b064a4da1e79a258433c5baf53061
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-02-18 18:40:01 +0100

    fetched data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react';
-import Places from './Places.jsx';
-
-
+import { useState } from "react";
+import Places from "./Places.jsx";
+import { useEffect } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [availablePlaces, setAvailablePlaces] = useState([])
+  const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  
+  useEffect(() => {
+    fetch("http://localhost:3000/places").then((response) => {
+      return response.json();
+    }).then((resData) => {
+      setAvailablePlaces(resData.places)
+    });
+  }, []);
+
 
   return (
     <Places
